9ccd56104a8d28c6213445fa09aa703f44c6e0bd
Alex Adamovic <alexanderadamovic@gmail.com>
2022-01-13 13:06:49 -0800

    add javascript functionality to form
    Co-authored-by: Zachary Green <ultrazacky@gmail.com>
    Co-authored-by: Anastasiia Riabets <anastasiia.ria@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
 
-$("#date-from").submit(function(event){
-  event.preventDefault();
+  $("form#date-form").submit(function(event){
+    const dayNight = $("select#day-night").val();
+    const cityTown = $("select#city-town").val();
 
-});
+    $("#celeb").removeAttr("class");
+    if (dayNight === 'day' && cityTown === 'city') {
+      $(".celebrity").show();
+      $("#celeb").addClass("anya");
+    } else if (dayNight === "night" && cityTown === "city") {
+      $(".celebrity").show();
+      $("#celeb").addClass("keanu");
+    } else if (dayNight === "day" && cityTown === "town") {
+      $(".celebrity").show();
+      $("#celeb").addClass("ingrid");
+    } else if (dayNight === "night" && cityTown === "town") {
+      $(".celebrity").show();
+      $("#celeb").addClass("tilda");
+    } else {
+      $(".celebrity").hide();
+      $("#pick").show();
+    }
 
+    event.preventDefault();
+  });
 });
